baf6fc6a397d31e7321cd11473241ff795e49e31
lmsand <lmsand99@icloud.com>
2024-03-14 18:35:46 -0500

    Fetch movie details

--- api/movies.ts
+++ api/movies.ts
@@ -1,14 +1,15 @@
 
+const headers = {
+  accept: 'application/json',
+  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWQ2ODczYTY2YjkyMmE4YjAzYzgzNGJjODU1NGJmZiIsInN1YiI6IjYzNzZlZDRjMjU1ZGJhMDA3ZjkwNTkxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rT4RGso23vCshFKDfwiN-UHcKu_AeIECKifK8Wz6NRg'
+}
+
 export const fetchTopRatedMovies = async () => {
   const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   const options = {
     method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWQ2ODczYTY2YjkyMmE4YjAzYzgzNGJjODU1NGJmZiIsInN1YiI6IjYzNzZlZDRjMjU1ZGJhMDA3ZjkwNTkxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rT4RGso23vCshFKDfwiN-UHcKu_AeIECKifK8Wz6NRg'
-    }
+    headers: headers
   };
-
 
     const res = await fetch(url, options)
 
@@ -19,13 +20,29 @@ export const fetchTopRatedMovies = async () => {
     const json = await res.json()
 
     return json.results
-
-
-
-
   // fetch(url, options)
   //   .then((res) => res.json())
   //   .then((json) => console.log(json))
   //   .catch((err) => console.error('error:' + err));
+
+}
+
+export const fetchMovie = async (id: number) => {
+
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers
+  };
+
+  const res = await fetch(url, options)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch movies')
+  }
+
+  const json = await res.json()
+
+  return json
 
 }
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,11 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
+import { useQuery } from '@tanstack/react-query'
+import { fetchMovie } from '@/api/movies'
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams()
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['movies', id],
+    queryFn:() => fetchMovie(id)
+  })
+
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+
+  if (error) {
+    return <Text>Failed to fetch data</Text>
+  }
+
   return (
     <View>
-      <Text>MovieDetails</Text>
+      <Text style={{ fontSize: 24, fontWeight: '500' }}>{data.title}</Text>
     </View>
   )
 }
